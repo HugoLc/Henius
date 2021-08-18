0d344e18f2da769269514abdc71c2968e9911b60
Hugo LC <hugolindoso.c@gmail.com>
2021-08-17 22:39:11 -0300

    Declaradas as funções pertinentes ao jogo #mostrarSequencia #checarResposta #finalizarJogo. Pendente implementar regra de negocio

--- js/jogo.js
+++ js/jogo.js
@@ -1,6 +1,7 @@
 class Jogo{
-  #botoes = [];
-  #sequencia = [];
+  const #botoes = [];
+  let #sequencia = [];
+  let #resposta =[];
   constructor(botoes){
     this.#botoes = botoes;
   }
@@ -8,14 +9,35 @@ class Jogo{
   function iniciarJogo(){
     let min = 1;
     let max = this.botoes.lenght;
-    let indice = #gerarRandom(min, max);
-    #sequencia.push(indice);
+    let gameOn = true;
+
+    while (gameOn) {
+      let indice = #gerarRandom(min, max);
+      #sequencia.push(indice);
+      #mostrarSequencia(#sequencia, #botoes);
+      #checarResposta(#sequencia, #resposta)
+    }
+
   }
 
   function #gerarRandom(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function #mostrarSequencia(seq, bt){
+
+  }
+
+  function #checarResposta(seq){
+    if (!true) {
+      //gameOn = false;
+    }
+  }
+
+  function #finalizarJogo(){
+    //registrar classificação
   }
 
 }
